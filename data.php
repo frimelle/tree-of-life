061fe8e6cd2940805f1ca8be69f9bb1ff520434e
lucie <lucie.kaffee@wikimedia.de>
2014-12-17 15:23:23 +0100

    add iframe for wikipedia/wikidata articles

--- data.php
+++ data.php
@@ -6,98 +6,108 @@
  * @author Lucie-Aimée Kaffee
  **/
 
-    require_once('api-request.php');
+	require_once('api-request.php');
 
-    $db = mysqli_connect( "localhost", "root", "password", "tree_db2" );
-    if (!$db) {
-        echo mysqli_connect_error();
-        return;
-    }
-    
-    session_start();
-    if( !isset($_SESSION['lang'])) {
-        $lang = 'en';
-    } else {
-        $lang = $_SESSION['lang'];
-    }
+	$db = mysqli_connect( "localhost", "root", "password", "tree_db" );
+	if (!$db) {
+		echo mysqli_connect_error();
+		return;
+	}
+	
+	session_start();
+	if( !isset($_SESSION['lang'])) {
+		$lang = 'en';
+	} else {
+		$lang = $_SESSION['lang'];
+	}
 
-    ////////////////for testing stuff
-    //$_GET['entity_id'] = 'Q1002125';
-    //////////////////////////////
-
-
-    $root = 'Q2382443';
-    $root_name_default = 'biota';
-    $root_link_default = 'http://www.wikidata.org/wiki/' . $root;
+	////////////////for testing stuff
+	//$_GET['entity_id'] = 'Q1002125';
+	//////////////////////////////
 
 
-    #check if this is the first node, if it is, use the root
-    if (!isset($_GET['entity_id']) || $_GET['entity_id'] == '#') {
-        $root_name = get_label($root, $lang);
-        
-        if ($root_name == "") {
-            $root_name = $root_name_default;
-        } 
-        $root_link = get_wiki_url($root, $lang);
-        if ($root_link == "") {
-            $root_link = $root_name_default;
-        } 
-
-        echo '[{"text":"' . $root_name . '","children":true, "id":"' . $root . '", "link":"' . $root_link . '"}]';
-    } else {
-        #get the entity_id of the node that was clicked
-        $parent = $_GET['entity_id'];
-        $qstring = "SELECT * FROM node WHERE parent='$parent'";
-        $qresult = mysqli_query( $db, $qstring );
-        $new_child = array();
-        $children = array();
-        $ids = array();
-
-        while( $row = mysqli_fetch_object( $qresult ) ) {
-            
-            $entity_id = $row->child;
-            $parent = $row->parent;
-            $has_children = $row->hasChildren; //this is onle for Tree.py not Tree-quick.php 
-
-            array_push($ids, $entity_id);
+	$root = 'Q2382443';
+	$root_name_default = 'biota';
+	$root_link_default = 'http://www.wikidata.org/wiki/' . $root;
 
 
-            $link = 'http://www.wikidata.org/wiki/' . $entity_id;
+	#check if this is the first node, if it is, use the root
+	if (!isset($_GET['entity_id']) || $_GET['entity_id'] == '#') {
+		$root_name = get_label($root, $lang);
+		
+		if ($root_name == "") {
+			$root_name = $root_name_default;
+		} 
+		$root_link = get_wiki_url($root, $lang);
+		if ($root_link == "") {
+			$root_link = $root_name_default;
+		} 
 
-            $new_child['text'] = $entity_id;
+		echo '[{"text":"' . $root_name . '","children":true, "id":"' . $root . '", "link":"' . $root_link . '"}]';
+	} else {
+		#get the entity_id of the node that was clicked
+		$parent = $_GET['entity_id'];
+		$qstring = "SELECT * FROM node WHERE parent='$parent'";
+		$qresult = mysqli_query( $db, $qstring );
+		$new_child = array();
+		$children = array();
+		$ids = array();
 
-            if ($has_children = 1) {
-                $new_child['children'] = true; 
-            } else {
-                $new_child['children'] = false;
-            }
-            $new_child['id'] = $entity_id;
-            $a_attr_elements['href'] = $link;
-            $new_child['a_attr'] = $a_attr_elements;
-            array_push($children, $new_child);
+		while( $row = mysqli_fetch_object( $qresult ) ) {
+			
+			$entity_id = $row->child;
+			$parent = $row->parent;
+			$has_children = $row->hasChildren; //this is onle for Tree.py not Tree-quick.php 
 
-        }
-        if( sizeof($ids) < 50) {
-            $content = get_label_url($ids, $lang);
-        } else {
-            //handle if there are more than 50 entity-ids (wikidata api takes only up to 50)
-        }
-        for ( $i = 0; $i < sizeof($children); $i++ ) {
-            
-            if( array_key_exists('labels', $content['entities'][$children[$i]['id']])) {
-                if ( array_key_exists( $lang, $content['entities'][$children[$i]['id']]['labels'] ) && array_key_exists( 'value', $content['entities'][$children[$i]['id']]['labels'][$lang] ) ) {
-                    
-                    $children[$i]['text'] = $content['entities'][$children[$i]['id']]['labels'][$lang]['value'];
-                }
-            }
+			array_push($ids, $entity_id);
 
-            if(array_key_exists('sitelinks', $content['entities'][$children[$i]['id']])) {
-                if ( array_key_exists( $lang . 'wiki', $content['entities'][$children[$i]['id']]['sitelinks'] ) && array_key_exists( 'url', $content['entities'][$children[$i]['id']]['sitelinks'][$lang . 'wiki'] ) ) {
-                    
-                    $children[$i]['a_attr']['href'] = $content['entities'][$children[$i]['id']]['sitelinks'][$lang . 'wiki']['url'];
-                }
-            }
-        }
-        
-        echo json_encode($children);
-    }
+
+			$link = 'https://m.wikidata.org/wiki/' . $entity_id . '?useskin=mobil&mobileaction=toggle_view_mobile';
+
+			$new_child['text'] = $entity_id;
+
+			if ($has_children = 1) {
+				$new_child['children'] = true; 
+			} else {
+				$new_child['children'] = false;
+			}
+			$new_child['id'] = $entity_id;
+			$a_attr_elements['href'] = $link;
+			$new_child['a_attr'] = $a_attr_elements;
+			array_push($children, $new_child);
+
+		}
+
+		$content = array();
+
+		if( sizeof($ids) < 50) {
+			$content = get_label_url( $ids, $lang );
+		} else {
+
+			$id_arrays = array_chunk( $ids, 49 );
+			$contents = array();
+
+			foreach( $id_arrays as $ids ) {
+				array_push( $contents, get_label_url( $ids, $lang ) );
+			}
+			$content = array_merge_recursive($contents);
+
+		}
+		for ( $i = 0; $i < sizeof($children); $i++ ) {
+			if( array_key_exists( 'labels', $content['entities'][$children[$i]['id']] ) ) {
+				if ( array_key_exists( $lang, $content['entities'][$children[$i]['id']]['labels'] ) && array_key_exists( 'value', $content['entities'][$children[$i]['id']]['labels'][$lang] ) ) {
+					
+					$children[$i]['text'] = $content['entities'][$children[$i]['id']]['labels'][$lang]['value'];
+				}
+			}
+
+			if(array_key_exists('sitelinks', $content['entities'][$children[$i]['id']])) {
+				if ( array_key_exists( $lang . 'wiki', $content['entities'][$children[$i]['id']]['sitelinks'] ) && array_key_exists( 'url', $content['entities'][$children[$i]['id']]['sitelinks'][$lang . 'wiki'] ) ) {
+					
+					$children[$i]['a_attr']['href'] = $content['entities'][$children[$i]['id']]['sitelinks'][$lang . 'wiki']['url'] . '?useskin=mobil&mobileaction=toggle_view_mobile';
+				}
+			}
+		}
+		
+		echo json_encode($children);
+	}
